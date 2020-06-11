b18da33186c98b42517026e8a65f3ebc0019ecce
Dawson Ren <dawsonren@gmail.com>
2020-06-11 15:50:43 -0500

    Cleaned Up

--- gui.py
+++ gui.py
@@ -45,27 +45,11 @@ class UniverseApp(arcade.Window):
         self.selectedStarSeed1 = 0
         self.selectedStarSeed2 = 0
 
-    def draw_universe(self):
-        arcade.start_render()
-
-        # iterate over all screen sectors
-        for screen_x in range(SECTORS_X):
-            for screen_y in range(SECTORS_Y):
-                seed1 = self.galaxy_offset["x"] + screen_x
-                seed2 = self.galaxy_offset["y"] + screen_y
-
-                star = StarSystem(seed1, seed2)
-
-                if star.star_exists:
-                    arcade.draw_circle_filled(screen_x * 16, screen_y * 16,
-                                              star.star_diameter / 2, star.star_color)
-
-        arcade.finish_render()
-
     def on_draw(self):
         """Called whenever you draw your window"""
         # Clear the screen and start drawing
-        pass
+        arcade.start_render()
+        arcade.draw_circle_filled(300, 300, 5, arcade.color.WHITE)
 
     def on_update(self, delta_time: float):
         """Handles the screen that pops up for selected stars"""
@@ -101,26 +85,8 @@ class UniverseApp(arcade.Window):
         elif symbol == 100:  # "d"
             self.right_pressed = True
 
-        if self.starSelected:
-            # generate full star system
-            star = StarSystem(self.selectedStarSeed1, self.selectedStarSeed2, True)
-            arcade.draw_rectangle_filled(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4,
-                                         SCREEN_WIDTH - 5, (SCREEN_HEIGHT / 2) - 5,
-                                         arcade.color.DARK_BLUE)
-            arcade.draw_rectangle_outline(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4,
-                                          SCREEN_WIDTH, (SCREEN_HEIGHT / 2),
-                                          arcade.color.WHITE, 5)
-
-            # x and y position of where the star will go
-            star_body = [SCREEN_WIDTH / 10, SCREEN_HEIGHT / 4]
-            star_body[0] += star.star_diameter * 1.375
-            # draw the star
-            # TODO: Finish drawing the stuff
-            # arcade.draw_circle_filled()
-
     def on_key_release(self, symbol: int, modifiers: int):
         """Resets speed to 0"""
-        print(self.galaxy_offset)
 
         if symbol == 119:  # "w"
             self.up_pressed = False
@@ -133,45 +99,11 @@ class UniverseApp(arcade.Window):
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         """Called when the user moves the mouse"""
-
-        arcade.start_render()
-        mouse = [x // 16, y // 16]
-
-        # iterate over all screen sectors
-        for screen_x in range(SECTORS_X):
-            for screen_y in range(SECTORS_Y):
-                seed1 = self.galaxy_offset["x"] + screen_x
-                seed2 = self.galaxy_offset["y"] + screen_y
-
-                star = StarSystem(seed1, seed2)
-
-                if star.star_exists:
-                    arcade.draw_circle_filled(screen_x * 16, screen_y * 16,
-                                              star.star_diameter / 8, star.star_color)
-
-                    # highlight hovered star
-                    if mouse[0] == screen_x and mouse[1] == screen_y:
-                        arcade.draw_circle_outline(screen_x * 16, screen_y * 16,
-                                                   (star.star_diameter / 8) + 4,
-                                                   arcade.color.YELLOW)
-        arcade.finish_render()
+        pass
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         """Called when the mouse is pressed"""
-
-        print(x, y)
-
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            seed1 = self.galaxy_offset["x"] + x
-            seed2 = self.galaxy_offset["y"] + y
-
-            star = StarSystem(seed1, seed2)
-            if star.star_exists:
-                self.starSelected = True
-                self.selectedStarSeed1 = seed1
-                self.selectedStarSeed2 = seed2
-            else:
-                self.starSelected = False
+        pass
 
 
 if __name__ == "__main__":
